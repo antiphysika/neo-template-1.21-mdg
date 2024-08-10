33d900d66def6efb068332544cd2f47d6e35d4d7
Francis <francis@beef.core.destinatech>
2024-08-10 16:34:57 +0100

    Add example testcase

---
+++ src/test/java/antiphysika/template/TestExample.java
@@ -0,0 +1,16 @@
+package antiphysika.template;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestExample
+{
+  @Test
+  public void testExample ()
+  {
+    assertTrue(true);
+  }
+}
+
+//
+// vim: ts=2 sw=2 et fdm=marker :
